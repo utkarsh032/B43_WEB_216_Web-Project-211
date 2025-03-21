47a7b5a66cc8464176f9549b6ff56a971dfec1e9
utkarsh raj <utkarshraj525@gmail.com>
2025-03-21 20:09:45 +0530

    Store API

--- server/Server.js
+++ server/Server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 // Enable Cors
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173" || "https://b43-web-216-web-project-211-1.onrender.com",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true
 }));
